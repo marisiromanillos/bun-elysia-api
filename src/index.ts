9c29717f0d2947f49bc2c979fa0dd2b9c466bc33
marisiromanillos <marisiromanillos@gmail.com>
2025-04-10 19:24:04 +0100

    change user route to route note

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { Elysia, t } from "elysia";
 import swagger from "@elysiajs/swagger";
-import { user } from "./routes/user";
-class User {
-  constructor(public data: string[] = ["Marisi", "Gonzalez"]) {}
-}
+import { note } from "./routes/note";
 
-const app = new Elysia().use(swagger()).use(user).listen(3000);
+const app = new Elysia().use(swagger()).use(note).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
